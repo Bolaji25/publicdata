eee24a3b792fe21881b6eb0f1b4db55a1d758f3e
jsemple19 <semple.j@gmail.com>
2019-10-11 11:25:16 +0200

    adding rex motifs genomewide

--- rexsitecreation.R
+++ rexsitecreation.R
@@ -7,6 +7,9 @@ library(ggbio)
 # BiocManager::install(c("gdata","GenomicRanges","BSgenome.Celegans.UCSC.ce11",
 # "rtracklayer","ggbio"))
 
+#########################
+# Albritton 2017 rex sites
+#########################
 
 # get rex sites from Albritton paper
 Albritton_elife2017="https://elifesciences.org/download/aHR0cHM6Ly9jZG4uZWxpZmVzY2llbmNlcy5vcmcvYXJ0aWNsZXMvMjM2NDUvZWxpZmUtMjM2NDUtc3VwcDEtdjEuemlw/elife-23645-supp1-v1.zip?_hash=8w773VoPpu6mx1PUQppgaFNrEniZQDyGrnfBDR6QDt8%3D"
@@ -51,8 +54,56 @@ autoplot(rexGR_ce11,layout="karyogram",aes(color = strength.category,fill = stre
   theme_alignment() +
   ggtitle("Strength of rex sites")
 
-file.remove("Albritton_elife2017.zip")
+#file.remove("Albritton_elife2017.zip")
 file.remove("SupplementaryFile1D.xls")
+
+
+#########################
+# Albritton 2017 rex motifs (genome wide)
+#########################
+
+# get rex sites from Albritton paper
+Albritton_elife2017="https://elifesciences.org/download/aHR0cHM6Ly9jZG4uZWxpZmVzY2llbmNlcy5vcmcvYXJ0aWNsZXMvMjM2NDUvZWxpZmUtMjM2NDUtc3VwcDEtdjEuemlw/elife-23645-supp1-v1.zip?_hash=8w773VoPpu6mx1PUQppgaFNrEniZQDyGrnfBDR6QDt8%3D"
+download.file(Albritton_elife2017,destfile="./Albritton_elife2017.zip")
+unzip("./Albritton_elife2017.zip",files="Tables/SupplementaryFile1E.xlsx",junkpaths=T)
+rexMotifs<-read.xls("./SupplementaryFile1E.xlsx")
+
+# change chr names to UCSC formats
+rexMotifs$Chromosome<-gsub("CHROMOSOME_","chr",rexMotifs$Chromosome)
+rexMotifs$Chromosome<-gsub("MtDNA","M",rexMotifs$Chromosome)
+
+# make Granges
+rexMotifGR<-GRanges(seqnames=rexMotifs$Chromosome,
+               ranges=IRanges(start=rexMotifs$Start, end=rexMotifs$End),
+               strand=rexMotifs$Strand)
+
+rexMotifGR$name<-paste0(rexMotifs$Chromosome,"_",rexMotifs$Start)
+rexMotifGR$score<-rexMotifs$Score
+# get liftover chain file from UCSC
+#download.file("http://hgdownload.cse.ucsc.edu/goldenPath/ce10/liftOver/ce10ToCe11.over.chain.gz", destfile="./ce10ToCe11.over.chain.gz")
+#system("gunzip ./ce10ToCe11.over.chain.gz")
+chainFile<-import.chain("./ce10ToCe11.over.chain")
+
+#lift rex sites from ce10 to ce11
+rexMotifGR_ce11<-unlist(liftOver(rexMotifGR,chain=chainFile))
+# you need to make sure all chromosomes are present in the seqlevels and seqlengths (get from BSgenome)
+seqlevels(rexMotifGR_ce11)<-seqlevels(Celegans)
+seqlengths(rexMotifGR_ce11)<-seqlengths(Celegans)
+
+# export as bed
+export.bed(rexMotifGR_ce11,"rexMotifs_Albritton2017_ce11.bed","bed")
+
+# plot rex sites on chromosome layout
+# autoplot(rexMotifGR_ce11,layout="karyogram",aes(color = strength.category,fill = strength.category)) +
+#   theme_alignment() +
+#   ggtitle("Strength of rex sites")
+
+file.remove("Albritton_elife2017.zip")
+file.remove("SupplementaryFile1E.xlsx")
+
+
+
+
 
 #########################
 # Crane 2015 rex sites
